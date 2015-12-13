8e804c75385dd138c30d5b52bf7656ad7fdd652a
mr-tweaker <aniket.lamba@gmail.com>
2015-12-13 23:54:40 +0530

    Create full_armani.mk

---
+++ full_armani.mk
@@ -0,0 +1,18 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/xiaomi/armani/device.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/armani/armani-vendor.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := armani
+PRODUCT_NAME := full_armani
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := HM 1S
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=armani
